a3cf0a8b7ddd041a2ce9a239be59691df69f6bcb
Pirantel <max420652@gmail.com>
2024-04-26 17:25:01 +0500

    moved chat to main page

--- static/script.js
+++ static/script.js
@@ -271,4 +271,40 @@ document.addEventListener('DOMContentLoaded', function() {
             resetPopupContainer();
         });
     }
+    initChat();
 });
+
+
+function initChat(){
+    // const username = "{{ request.user.username }}";
+
+    const chatSocket = new WebSocket( // НЕ ТРОГАТЬ СУКА
+        'ws://'
+        + window.location.host
+        + '/ws/chat/'
+    )
+    chatSocket.onmessage = function(e) {
+        const data = JSON.parse(e.data);
+        document.querySelector('#chat-log').value += `${data.sender}: ${data.message}\n`;
+    }
+    chatSocket.onclose = function(e) {
+        console.error('Chat socket closed unexpectedly');
+    }
+    document.querySelector('#chat-message-input').focus();
+    document.querySelector('#chat-message-input').onkeyup = function(e) {
+        if (e.keyCode === 13) {  // enter, return
+            document.querySelector('#chat-message-submit').click();
+        }
+    }
+    document.querySelector('#chat-message-submit').onclick = function(e) {
+        const messageInputDom = document.querySelector('#chat-message-input');
+        const message = messageInputDom.value;
+        if (!message){
+            return;
+        }
+        chatSocket.send(JSON.stringify({
+            'message': message
+        }));
+        messageInputDom.value = '';
+    };
+}
