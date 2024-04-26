999695f6bf7a143e3a3e59f03eeedbd1b981cd7a
Pirantel <max420652@gmail.com>
2024-04-26 22:37:16 +0500

    tweaks

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 CLIENT_ID = '453737687894-c3hei64hc4tsimbqfs6v4iehtagt955d.apps.googleusercontent.com'
 CLIENT_SECRET = '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'
-SERVER_DOMAIN = '127.0.0.1:8000'
+SERVER_IP = '127.0.0.1'
+SERVER_DOMAIN = f'{SERVER_IP}:8000'
 REDIRECT_URI = f'http://{SERVER_DOMAIN}/auth'
