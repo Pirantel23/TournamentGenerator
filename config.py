5a57e84c01ed3daceb4b8c3883a4f2df20e38d76
Pirantel <max420652@gmail.com>
2024-05-02 21:58:38 +0500

    changed domain

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CLIENT_ID = '453737687894-c3hei64hc4tsimbqfs6v4iehtagt955d.apps.googleusercontent.com'
 CLIENT_SECRET = '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'
-SERVER_IP = '127.0.0.1'
+SERVER_IP = 't-gg.ru'
 SERVER_DOMAIN = f'{SERVER_IP}:8000'
 REDIRECT_URI = f'http://{SERVER_DOMAIN}/auth'
