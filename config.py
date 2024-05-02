6c8829165b033e1f225a3140de59c6048902755d
Pirantel <max420652@gmail.com>
2024-05-03 01:29:40 +0500

    changed redirect_uri

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ CLIENT_ID = '453737687894-c3hei64hc4tsimbqfs6v4iehtagt955d.apps.googleuserconten
 CLIENT_SECRET = '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'
 SERVER_IP = 't-gg.ru'
 SERVER_DOMAIN = f'{SERVER_IP}:8000'
-REDIRECT_URI = f'http://{SERVER_DOMAIN}/auth'
+REDIRECT_URI = f'https://{SERVER_IP}/auth'
