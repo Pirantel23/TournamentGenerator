02ab8c22d966d30e44c2adc8c407acfe24ab1358
Pirantel <max420652@gmail.com>
2024-05-03 16:15:55 +0500

    DEPLOOOOOOOOOOOOY

--- TournamentGenerator/settings.py
+++ TournamentGenerator/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from config import SERVER_IP
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,12 +135,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static_dir/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'rootstatic')
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static_dir",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
--- static/script.js
+++ static/script.js
@@ -50,9 +50,9 @@ function changeImage() {
     }
 
     if (tournamentType === 'single') {
-        tournamentImg.src = '/static_dir/single-elimination.svg';
+        tournamentImg.src = 'static/single-elimination.svg';
     } else if (tournamentType === 'double') {
-        tournamentImg.src = '/static_dir/double-elimination.svg';
+        tournamentImg.src = 'static/double-elimination.svg';
     }
 }
 
