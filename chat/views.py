b267ab725770503bbe98aaf2a3252f473ca33820
Pirantel <max420652@gmail.com>
2024-05-04 17:21:57 +0500

    added shit logging

--- chat/views.py
+++ chat/views.py
@@ -26,7 +26,7 @@ def send_message(request):
 def long_poll_messages(request):
     if request.method != 'POST':
         return JsonResponse({'error': 'POST request required.'}, status=400)
-    print(f'Long poll request received from {request.user}.')
+    open('log.txt', 'a').write(f'Long poll request received from {request.user}.\n')
     request_time = time.time()
     sender = request.user
     data = json.loads(request.body)
@@ -42,7 +42,7 @@ def long_poll_messages(request):
         new_messages = Message.objects.filter(id__gt=last_message_id, room=room).exclude(sender=sender)
         if new_messages:
             messages_data = [{'id': msg.id, 'sender': msg.sender.username, 'content': msg.content, 'room': msg.room, 'timestamp': msg.timestamp, 'type': msg.type} for msg in new_messages]
-            print(f'Long poll response sent to {request.user} in {time.time() - request_time:.2f}s')
+            open('log.txt', 'a').write(f'Long poll response sent to {request.user} in {current - request_time:.2f}s\n')
             return JsonResponse(messages_data, safe=False)
         else:
             time.sleep(1)
